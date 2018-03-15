0d206680db0f940358fb7b0663b36a71ac275d42
Bo Kleynen <kleynenbo@gmail.com>
2018-03-15 14:50:26 +0100

    add extension to BufferedInputStream

---
+++ src/http_message/BufferedInputStream.java
@@ -0,0 +1,31 @@
+package http_message;
+
+import java.io.InputStream;
+import java.io.IOException;
+
+import static http_message.HTTPMessage.CRLF;
+
+public class BufferedInputStream extends java.io.BufferedInputStream {
+
+    /**
+     * Constructs a buffered input stream from a given input stream.
+     * @param in the given input stream to construct from
+     */
+    public BufferedInputStream(InputStream in) {
+        super(in);
+    }
+
+    /**
+     * Reads a single line (terminated by CRLF) from the input stream.
+     * @return the line which was read
+     * @throws IOException if the line could not be read
+     */
+    public String readLine() throws IOException {
+        String output = "";
+        do
+            output += (char) this.read();
+        while(!output.endsWith(CRLF));
+
+        return output.substring(0, output.length() - 2);
+    }
+}
