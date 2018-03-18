5b2c1c321c52aa2284761f004ee65c26b85240f6
Bo Kleynen <kleynenbo@gmail.com>
2018-03-18 10:19:45 +0100

    add server by Maarten

---
+++ src/server/TCPServer.java
@@ -0,0 +1,24 @@
+package server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TCPServer {
+	public static final int PORT = 4444;
+
+	public static void main(String[] args) throws IOException {
+		new TCPServer().runServer();
+	}
+	
+	public void runServer() throws IOException {
+		ServerSocket serverSocket = new ServerSocket(PORT);
+		System.out.println("Server up");
+		while (true) {
+			Socket socket = serverSocket.accept();
+			new ServerThread(socket).start();
+			
+		}
+	}
+
+}
