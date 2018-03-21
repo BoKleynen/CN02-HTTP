7bfc7b1b690ee15b9d6659e17d79ab89b21d8713
Bo Kleynen <kleynenbo@gmail.com>
2018-03-21 16:04:28 +0100

    change TestClient

--- src/client/TestClient.java
+++ src/client/TestClient.java
@@ -12,15 +12,27 @@ import static org.apache.commons.io.FileUtils.writeByteArrayToFile;
 @SuppressWarnings("all")
 public class TestClient {
     public static void main(String[] args) throws Exception {
-        URI uri = new URI("//localhost:4444/hgjhgkjh");
+        URI uri = new URI("http://localhost:4444");
 
         HTTPClientConnection client = new HTTPClientConnection(uri.getHost(), uri.getPort() != -1 ? uri.getPort() : 80);
         HTTPRequest request = new HTTPRequest("GET", uri);
         System.out.println(request);
         HTTPResponse response = client.sendRequest(request);
         response.print();
-        File file = new File("websites/" + uri.getAuthority() + request.getPath() + "a.html");
-        writeStringToFile(file, response.getBody());
+        String contentType = response.getHeader("Content-Type");
+        if (contentType != null && contentType.contains("html")) {
+            File file = new File("websites/" + uri.getAuthority() + "/" +
+                    (request.getPath().length() == 0 ? "index" : request.getPath())  + ".html");
+            writeStringToFile(file, response.getBody());
+        } else if (contentType != null && contentType.equals("text/plain")) {
+            File file = new File("websites/" + uri.getAuthority() + "/" +
+                    (request.getPath().length() == 0 ? "index" : request.getPath()) + ".txt");
+            writeStringToFile(file, response.getBody());
+        } else if (contentType != null && contentType.contains("image")) {
+            File file = new File("websites/" + uri.getAuthority() + "/" + request.getPath());
+            writeByteArrayToFile(file, Base64.getDecoder().decode(response.getBody()));
+        }
+
 
         // Gets the embedded objects
         HTTPRequest imgRequest;
@@ -31,8 +43,9 @@ public class TestClient {
                 System.out.println(imgRequest);
                 imgResponse = client.sendRequest(imgRequest);
                 imgResponse.print();
+                File file;
                 if (imgResponse.success()) {
-                    file = new File("websites/" + uri.getAuthority() + imgRequest.getPath());
+                    file = new File("websites/" + uri.getAuthority() + "/" + imgRequest.getPath());
                     writeByteArrayToFile(file, Base64.getDecoder().decode(imgResponse.getBody()));
                 }
             } catch (Exception e) {
